219d2362b75f8bc0909feb3aa60e95c7811472d3
FeliOdras <ariane.feldmann@odras.de>
2019-02-27 10:07:35 +0100

    Created class constructor

---
+++ assets/js/mygithub.js
@@ -0,0 +1,6 @@
+class MyGithub {
+    constructor() {
+        this.htmlContainer = document.querySelector("#myGithubOutput");
+        this.url = `https://api.github.com/users/FeliOdras/repos?client_id=fd294f0cd34bb0c9d185&client_secret=5429a69b75c88ca46305aafd53715532c56e9abf`
+    }
+}

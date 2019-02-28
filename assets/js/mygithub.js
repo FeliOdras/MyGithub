4ec564b6c9d610b3f36cb27585878f1b77d49639
FeliOdras <ariane.feldmann@odras.de>
2019-02-28 12:46:41 +0100

    Adjusted header styles

--- assets/js/mygithub.js
+++ assets/js/mygithub.js
@@ -1,93 +1,92 @@
 class MyGithub {
-  constructor(domselector) {
-    // debugger
-    this.htmlContainer = document.querySelector(domselector);
-    this.githubUser = `FeliOdras`;
-    this.apiUrl = `https://api.github.com/users/${
+    constructor(domselector) {
+        // debugger
+        this.htmlContainer = document.querySelector(domselector);
+        this.githubUser = `FeliOdras`;
+        this.apiUrl = `https://api.github.com/users/${
       this.githubUser
     }/repos?client_id=fd294f0cd34bb0c9d185&client_secret=5429a69b75c88ca46305aafd53715532c56e9abf`;
-    this.userApiUrl = `https://api.github.com/users/${
+        this.userApiUrl = `https://api.github.com/users/${
       this.githubUser
     }?client_id=fd294f0cd34bb0c9d185&client_secret=5429a69b75c88ca46305aafd53715532c56e9abf`;
-    this.requestData();
-    this.requestUserData();
-  }
+        this.requestData();
+        this.requestUserData();
+    }
 
-  requestData() {
-    fetch(this.apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        this.data = data;
-        this.render();
-      });
-  }
+    requestData() {
+        fetch(this.apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                this.data = data;
+                this.render();
+            });
+    }
 
-  requestUserData() {
-    fetch(this.userApiUrl)
-      .then(response => response.json())
-      .then(userData => {
-        this.userData = userData;
-        this.createHeader();
-      });
-  }
+    requestUserData() {
+        fetch(this.userApiUrl)
+            .then(response => response.json())
+            .then(userData => {
+                this.userData = userData;
+                this.createHeader();
+            });
+    }
 
-  //   fetch(this.userApiUrl)
-  //       .then(response => response.json())
-  //       .then(userData => {
-  //         this.data = userData;
-  //         this.render();
-  //       });
-  //   }
+    //   fetch(this.userApiUrl)
+    //       .then(response => response.json())
+    //       .then(userData => {
+    //         this.data = userData;
+    //         this.render();
+    //       });
+    //   }
 
-  createHeader() {
-    let user = this.userData;
-    let userLocation;
-    user.location != null
-      ? (userLocation = `<div id="userLocation">${user.location}</div>`)
-      : (userLocation = ``);
-    let userBio;
-    user.bio != null
-      ? (userBio = `<div id="userLocation">${user.bio}</div>`)
-      : (userBio = ``);
-    let userDataOutput = `
-    <div class="jumbotron text-center">
-    <div id="userAvatar" class="m-4"><img class="img-thumbnail rounded-circle" src="${
+    createHeader() {
+        let user = this.userData;
+        let userLocation;
+        user.location != null ?
+            (userLocation = `<div id="userLocation">${user.location}</div>`) :
+            (userLocation = ``);
+        let userBio;
+        user.bio != null ?
+            (userBio = `<div id="userLocation">${user.bio}</div>`) :
+            (userBio = ``);
+        let userDataOutput = `
+    <div class="jumbotron text-left bg-secondary text-white p-3 clearfix">
+    <div id="userAvatar" class="mr-4 p-3 text-center float-left border w-240px"><img class="img-thumbnail mb-3 rounded-circle" src="${
       user.avatar_url
-    }"></div>
-    <div id="userLogin" class="font-weight-bold">${user.login} </div>
-    <div class="text-muted small">(${
+    }"><br>
+    <span class="small">${userBio}</span>
+    </div>
+    <div id="userLogin" class="font-weight-bold text-warning">${
+      user.login
+    } </div>
+    <div class="text-light font-italic small">(${
       user.public_repos
     } public repositories)</div>
-    <div id="userName">${user.name}</div>
+    <div id="userName" class="mt-3">${user.name}</div>
     <div id="userLocation">${userLocation}</div>
-    <div id="userBio">${userBio}</div>
-    <div id="followers"><a href="${
-      user.followers_url
-    }" target="_blank">Followers: ${user.followers}</div>
-    <div id="following"><a href="${
-      user.following_url
-    }" target="_blank">Followers: ${user.following}</div>
+    <button id="followers" class="bg-warning d-inline p-1 small rounded border-0"><a href="https://github.com/${this.githubUser}?tab=followers" target="_blank" class="text-white text-decoration-none">Followers: ${user.followers}</button>
+    <button id="following" class="bg-warning d-inline p-1 small rounded border-0"><a href="https://github.com/${this.githubUser}?tab=following" target="_blank" class="text-white text-decoration-none">Following: ${user.following}</button>
     </div> `;
-    document.querySelector("#myGithubUser").innerHTML = userDataOutput;
-  }
+        document.querySelector("#myGithubUser").innerHTML = userDataOutput;
+    }
 
-  template() {
-    let repoList = this.data;
+    template() {
+        let repoList = this.data;
 
-    return repoList
-      .map(repo => {
-        let liveProjectLink;
-        repo.homepage != null
-          ? (liveProjectLink = `<a href="${
+        return repoList
+            .map(repo => {
+                let liveProjectLink;
+                repo.homepage != null ?
+                    (liveProjectLink = `<a href="${
               repo.homepage
-            }">View live application</a>`)
-          : (liveProjectLink = `<span class="not-available">No live application available</span>`);
-        let repoDescription;
-        repo.description != null
-          ? (repoDescription = `${repo.description}`)
-          : (repoDescription = `<span class="not-available">No description available</span>`);
-        return `
-            <div class="flexbox border m-1 p-2 w-25">
+            }">View live application</a>`) :
+                    (liveProjectLink = `<span class="not-available">No live application available</span>`);
+                let repoDescription;
+                repo.description != null ?
+                    (repoDescription = `${repo.description}`) :
+                    (repoDescription = `<span class="not-available">No description available</span>`);
+                return `
+            <div class="flexbox border m-1 p-2 w-240px">
             <div id="repoName"> ${repo.name}</div>
             <div id="repoDescription">${repoDescription}</div>
             <div id="repoLink"><a href="${
@@ -96,19 +95,21 @@ class MyGithub {
             <div id="liveAppLink">${liveProjectLink}</div>
             </div>
             `;
-      })
-      .join("");
-  }
+            })
+            .join("");
+    }
 
-  render() {
-    let output = "";
-    const template = this.template();
-    const flexboxLayoutTop = `<div class="d-flex flex-wrap justify-content-around">`;
-    output += flexboxLayoutTop;
-    output += template;
-    output += `</div>`;
-    this.htmlContainer.innerHTML = output;
-  }
+    render() {
+        let output = "";
+        let user = this.userData;
+        const template = this.template();
+        const flexboxLayoutTop = `<div class="d-flex flex-wrap justify-content-around">`;
+        output += `<h2>${user.login}'s repositories</h2>`
+        output += flexboxLayoutTop;
+        output += template;
+        output += `</div>`;
+        this.htmlContainer.innerHTML = output;
+    }
 }
 
 const myGithubRepos = new MyGithub("#myGithubOutput");
